b7eda1b5b7a784048c1d96cd1708fc1a19893c11
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-01 19:42:47 +0100

    ready to go

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ var lol = {
   name: 'hehe'
 }
 var arr
-console.log(arr)
-
 while (arr = r.exec(app.innerHTML)) {
   var s = app.innerHTML.replace(arr[0], lol[arr[0].substr(1,arr[0].length - 2)], arr[0])
   app.innerHTML = s
